89145f20d3368166f9d7335d20a9fea2640e149f
Matthew Walker <matthewahwalker@gmail.com>
2025-03-27 12:34:44 +0000

    remove jarvis json imports

--- SLME.py
+++ SLME.py
@@ -1,17 +1,14 @@
 from math import pi
 from scipy import constants
-from jarvis.db.jsonutils import dumpjson
-from jarvis.db.jsonutils import loadjson
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import math
 from scipy.integrate import simps
 
-class Efficiency(solar_path="AM15G.csv"):
+class Efficiency():
     def __init__(
-        self,
-    ):
+            self,
+            solar_path="AM15G.csv"):
         self.c = constants.c
         self.h = constants.h
         self.q = constants.e
@@ -68,11 +65,15 @@ class Efficiency(solar_path="AM15G.csv"):
         eta = max_power / self.p_in
         percentage_efficiency = eta*100
         if JV_plot:
-            V = np.linspace(0, 2, 200)
-            plt.plot(V, J(V))
-            plt.plot(V, power(V), linestyle="--")
-            plt.savefig("J_V_plot.svg")
-            plt.close()
+            V = np.linspace(0, test_voltage+0.1, 200)
+            fig, ax1 = plt.subplots()
+            ax2 = ax1.twinx()
+            ax1.plot(V,J(V),label='J(V)')
+            ax2.plot(V, power(V), color='tab:orange',label='P(V)')
+            ax1.set_ylabel(r'J / A m$^{-2}$')
+            ax1.set_xlabel('V / V')
+            ax2.set_ylabel(r'Power / W m$^{-2}$')
+            plt.savefig("JV_plot.svg")
         if display:
             print(f"Maximum predicted efficiency of {round(percentage_efficiency,2)}% corresponding to a power of {round(max_power,2)} W/m^2, short-circuit current density of {round(Jsc,2)} A/m^2 and voltage of {round(test_voltage,2)} V under AM1.5G illumination at {T} K for a {d} nm material")
         if return_PVJ:
